d0ec12bb96898cb657b13d7867686c716175e3df
Hannah Wilks <hannahwilks@Hannahs-MacBook-Air-2.local>
2020-11-12 11:38:16 -0500

    finished number generator

--- random number generator.py
+++ random number generator.py
@@ -1,16 +1,16 @@
 import random
-import sys
 print ("minimum:0")
 print ("maximum:50")
 x = random.randint(1,50)
-   # taking guessing number as input
-guess = int(input("Guess a number:- ")) 
-     
-    # Condition testing
-if x == guess:  
-       print("Congratulations you did it in ")
-       # Once guessed, loop will break 
-elif x > guess:
-       print("You guessed too small!")
-elif x < guess:
-       print("You Guessed too high!")
+guess=x + 1
+
+while guess != x:
+    # taking guessing number as input
+    guess = int(input("Guess a number:- "))  
+        # Condition testing
+    if x == guess:  
+        print("Congratulations you did it!")
+    elif x > guess:
+        print("You guessed too small!")
+    elif x < guess:
+        print("You guessed too high!")
