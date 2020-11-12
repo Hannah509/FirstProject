80ae89a3b871af08ebf981e333fc3d962746d51d
Hannah Wilks <hannahwilks@Hannahs-MacBook-Air-2.local>
2020-11-12 11:30:54 -0500

    random number generator code

--- random number generator.py
+++ random number generator.py
@@ -1,1 +1,16 @@
-print ("hello world")
+import random
+import sys
+print ("minimum:0")
+print ("maximum:50")
+x = random.randint(1,50)
+   # taking guessing number as input
+guess = int(input("Guess a number:- ")) 
+     
+    # Condition testing
+if x == guess:  
+       print("Congratulations you did it in ")
+       # Once guessed, loop will break 
+elif x > guess:
+       print("You guessed too small!")
+elif x < guess:
+       print("You Guessed too high!")
